d0159f815a9c632e962ff308f47d2a70263d948b
Matt Mason <mamaso@microsoft.com>
2017-06-28 16:32:51 -0700

    Updated worker with protobufjs & typescript

--- src/grpcMessageConverters.ts
+++ src/grpcMessageConverters.ts
@@ -1,145 +1,46 @@
-'use strict';
+import { FunctionRpc as messages } from '../protos/rpc.js';
 
-let messages = require('./protos/FunctionRpc_pb');
-
-function convertFromRpcHttp(rpcHttp) {
+function convertFromRpcHttp(rpcHttp: messages.RpcHttp$Properties) {
   // Get Request
-  let contextHttpRequest = {
-    headers: {},
-    query: {},
-    params: {}
+  let httpContext: any = {
+    method: rpcHttp.method,
+    originalUrl: rpcHttp.url,
+    headers: rpcHttp.headers,
+    query: rpcHttp.query,
+    params: rpcHttp.params,
+    rawBody: rpcHttp.rawBody,
+    body: convertFromTypedData(rpcHttp.body)
   };
 
-  contextHttpRequest.method = rpcHttp.getMethod();
-  contextHttpRequest.originalUrl = rpcHttp.getUrl();
-  let inputHttpRequestHeaders = rpcHttp.getHeadersMap().toObject();
-  for (let [key, value] of inputHttpRequestHeaders) {
-    contextHttpRequest.headers[key] = value;
-  }
-
-  let inputHttpQueryParams = rpcHttp.getQueryMap().toObject();
-  for (let [key, value] of inputHttpQueryParams) {
-    contextHttpRequest.query[key] = value;
-  }
-
-  if (rpcHttp.getRawBody()) {
-    contextHttpRequest.rawBody = rpcHttp.getRawBody();
-  }
-
-  let inputHttpRequestParams = rpcHttp.getParamsMap().toObject(false, messages.TypedData.toObject);
-  for (let [key, value] of inputHttpRequestParams) {
-    contextHttpRequest.params[key] = value;
-  }
-  if (rpcHttp.getBody()) {
-    contextHttpRequest.body = this.convertFromTypedDataObject(rpcHttp.getBody());
-  }
-  return contextHttpRequest;
+  return httpContext;
 }
 
 function convertToRpcHttp(inputMessage) {
   let isRawResponse = true;
-  let httpMessage = new messages.RpcHttp();
-  if (inputMessage['method']) {
-    httpMessage.setMethod(inputMessage['method']);
-    isRawResponse = false;
-  }
-  if (inputMessage['rawBody']) {
-    httpMessage.setRawBody(inputMessage['rawBody']);
-    isRawResponse = false;
-  }
-  if (inputMessage['originalUrl']) {
-    httpMessage.setUrl(inputMessage['originalUrl']);
-    isRawResponse = false;
-  }
-  if (inputMessage['headers']) {
-    for (let headerKey in inputMessage['headers']) {
-      httpMessage.getHeadersMap().set(headerKey, inputMessage['headers'][headerKey]);
-    }
-    isRawResponse = false;
-  }
-  if (inputMessage['query']) {
-    for (let queryKey in inputMessage['query']) {
-      httpMessage.getQueryMap().set(queryKey, inputMessage['query'][queryKey]);
-    }
-    isRawResponse = false;
-  }
-  if (inputMessage['statusCode']) {
-    httpMessage.setStatusCode(inputMessage['statusCode'].toString());
-    isRawResponse = false;
-  }
-  if (inputMessage['status'] && !httpMessage.getStatusCode()) {
-    httpMessage.setStatusCode(inputMessage['status'].toString());
-    isRawResponse = false;
-  }
-  if (inputMessage['body']) {
-    isRawResponse = false;
-    let httpBody = new messages.TypedData();
-    if (inputMessage['isRaw']) {
-      httpMessage.setIsRaw(true);
-    }
-    if (inputMessage['headers'] && inputMessage['headers']['raw']) {
-      httpBody.setTypeVal(2);
-    }
-    if (Buffer.isBuffer(inputMessage['body']) || ArrayBuffer.isView(inputMessage['body'])) {
-      httpBody.setTypeVal(2);
-    }
-    if (httpBody.getTypeVal() === 2 && !inputMessage['rawBody']) {
-      httpBody.setBytesVal(inputMessage['body']);
-    } else {
-      httpBody.setStringVal(this.getStringForObject(inputMessage['body']));
-    }
-    httpMessage.setBody(httpBody);
-  }
 
-  if (isRawResponse) {
-    let rawResponseTypedData = new messages.TypedData();
-    if (Buffer.isBuffer(inputMessage) || ArrayBuffer.isView(inputMessage)) {
-      rawResponseTypedData.setTypeVal(2);
-      rawResponseTypedData.setBytesVal(inputMessage);
-    } else {
-      rawResponseTypedData.setTypeVal(0);
-      rawResponseTypedData.setStringVal(this.getStringForObject(inputMessage));
-    }
-    httpMessage.setRawResponse(rawResponseTypedData);
-  }
+  let httpMessage: messages.RpcHttp$Properties = inputMessage;
+  httpMessage.statusCode = (inputMessage.statusCode || inputMessage.status).toString();
+
+  httpMessage.body = getTypedDataFromObject(inputMessage.body);
   return httpMessage;
 }
 
-function convertFromTypedData(typedData) {
+function convertFromTypedData(typedData: messages.TypedData$Properties = {}) {
   switch (typedData.typeVal) {
-    case 0:
-    case 1:
+    case messages.TypedData.Type.String:
+    case messages.TypedData.Type.Json:
     default: {
       let parsedJson = typedData.stringVal;
       try {
-        parsedJson = JSON.parse(typedData.stringVal);
+        parsedJson = JSON.parse(<string>typedData.stringVal);
         return parsedJson;
       } catch (error) {
         return parsedJson;
       }
     }
-    case 2: {
-      return Buffer.from(typedData.bytesVal);
+    case messages.TypedData.Type.Bytes: {
+      return new Buffer(<Uint8Array>typedData.bytesVal);
     }
-  }
-}
-
-function convertFromTypedDataObject(typedData) {
-  switch (typedData.getTypeVal()) {
-    case 0:
-    case 1:
-    default:
-      {
-        try {
-          return JSON.parse(typedData.getStringVal());
-        } catch (error) {
-          return typedData.getStringVal();
-        }
-      }
-    case 2:
-      {
-        return Buffer.from(typedData.getBytesVal());
-      }
   }
 }
 
@@ -158,42 +59,29 @@ function getStringForObject(inputObject) {
   }
 }
 
-function getTypedDataFromObject(inputObject) {
-  let typedData = new messages.TypedData();
-  if (typeof (inputObject) === 'string') {
-    typedData.setTypeVal(0);
-    typedData.setStringVal(inputObject);
+function getTypedDataFromObject(inputObject): messages.TypedData$Properties {
+  if (typeof inputObject === 'string') {
+    return {
+      typeVal: messages.TypedData.Type.String,
+      stringVal: inputObject
+    };
   } else if (Buffer.isBuffer(inputObject) || ArrayBuffer.isView(inputObject)) {
-    typedData.setTypeVal(2);
-    typedData.setBytesVal(inputObject);
+    return {
+      typeVal: messages.TypedData.Type.Bytes,
+      bytesVal: new Uint8Array(inputObject.buffer)
+    };
   } else {
-    // TODO do we need JSON type?
-    typedData.setTypeVal(0);
-    typedData.setStringVal(JSON.stringify(inputObject));
+    return {
+      typeVal: messages.TypedData.Type.String,
+      stringVal: JSON.stringify(inputObject)
+    }
   }
-  return typedData;
-}
-
-function getUnpackedMessage(anyTypeMessage, messageType, messageTypeName) {
-  let typeName = 'FunctionRpc.' + messageTypeName;
-  let unpackedValue = anyTypeMessage.unpack(messageType.deserializeBinary, typeName);
-  return unpackedValue;
-}
-
-function getPackedMessage(message, messageTypeName) {
-  let typeName = 'FunctionRpc.' + messageTypeName;
-  let packedMessage = new proto.google.protobuf.Any();
-  packedMessage.pack(message.serializeBinary(), typeName);
-  return packedMessage;
 }
 
 module.exports = {
   convertFromRpcHttp: convertFromRpcHttp,
   convertToRpcHttp: convertToRpcHttp,
   convertFromTypedData: convertFromTypedData,
-  convertFromTypedDataObject: convertFromTypedDataObject,
   getStringForObject: getStringForObject,
-  getTypedDataFromObject: getTypedDataFromObject,
-  getUnpackedMessage: getUnpackedMessage,
-  getPackedMessage: getPackedMessage
+  getTypedDataFromObject: getTypedDataFromObject
 };
--- src/grpcMessageHandlers.ts
+++ src/grpcMessageHandlers.ts
@@ -1,209 +1,144 @@
-'use strict';
-
 const grpcMessageConverters = require('./grpcMessageConverters');
-let messages = require('./protos/FunctionRpc_pb');
-let createFunction = require('./azurefunctions/functions.js').createFunction;
+import { FunctionRpc as messages } from '../protos/rpc.js'; 
+import { EventStream } from './rpcService';
+let createFunction = require('../azurefunctions/functions.js').createFunction;
 
-function handleFunctionLoadRequest(functionLoadRequest, loadedFunctionsList) {
-  let rpcFunctionMetadata = functionLoadRequest.getMetadata();
-  let functionMetadata = {};
-  functionMetadata.name = rpcFunctionMetadata.getName();
-  functionMetadata.directory = rpcFunctionMetadata.getDirectory();
-  if (rpcFunctionMetadata.getScriptFile()) {
-    functionMetadata.script_file = rpcFunctionMetadata.getScriptFile();
+function handleFunctionLoadRequest(functionLoadRequest: messages.FunctionLoadRequest, loadedFunctionsList) {
+  if (functionLoadRequest.functionId && functionLoadRequest.metadata) {
+    loadedFunctionsList[functionLoadRequest.functionId] = functionLoadRequest.metadata;
   }
-  if (rpcFunctionMetadata.getEntryPoint()) {
-    functionMetadata.entry_point = rpcFunctionMetadata.getEntryPoint();
-  }
-  loadedFunctionsList[functionLoadRequest.getFunctionId()] = functionMetadata;
   return loadedFunctionsList;
 }
 
-function handleInvokeRequest(invocationRequest, call, requestId, loadedFunctionsList) {
+function handleInvokeRequest(invocationRequest: messages.InvocationRequest, call: EventStream, requestId, loadedFunctionsList: messages.RpcFunctionMetadata$Properties[]) {
   // TODO handle updating non-existing function_id
-  let functionMetadata = loadedFunctionsList[invocationRequest.getFunctionId()];
-  let scriptFilePath = functionMetadata.script_file;
-  let invocationResponse = new messages.InvocationResponse();
-  let statusResult = new messages.StatusResult();
+  if (!invocationRequest.functionId || !invocationRequest.triggerMetadata) {
+    throw new Error("Invalid invocation");
+  }
 
-  let context = {
-    executionContext: {},
+  let functionMetadata: messages.RpcFunctionMetadata$Properties = loadedFunctionsList[invocationRequest.functionId];
+  let scriptFilePath = <string>functionMetadata.scriptFile;
+
+  let context: any = {
+    invocationId: invocationRequest.invocationId,
+    _entryPoint: functionMetadata.entryPoint,
+    executionContext: {
+      invocationId: invocationRequest.invocationId,
+      functionName: functionMetadata.name,
+      functionDirectory: functionMetadata.directory
+    },
     bindings: {},
     bindingData: {},
     req: {},
     _inputs: []
   };
 
-  invocationResponse.setInvocationId(invocationRequest.getInvocationId());
-
-  context.invocationId = invocationRequest.getInvocationId();
-  context.executionContext.invocationId = invocationRequest.getInvocationId();
-  context.executionContext.functionName = functionMetadata.name;
-  context.executionContext.functionDirectory = functionMetadata.directory;
-
-  // Get bindings
-  if (functionMetadata.entry_point) {
-    context._entryPoint = functionMetadata.entry_point;
-  }
-
+  let bindingData = invocationRequest.triggerMetadata;
   // Get bindingData from trigger_metadata
-
-  let triggerMetadata = invocationRequest.getTriggerMetadataMap().toObject(false, messages.TypedData.toObject);
-  for (let key in triggerMetadata) {
-    context.bindingData[triggerMetadata[key][0]] = grpcMessageConverters.convertFromTypedData(triggerMetadata[key][1]);
+  for (let key in bindingData) {
+    context.bindingData[key] = grpcMessageConverters.convertFromTypedData(bindingData[key]);
   }
 
   // Get _inputs from ParameterBindings
-  let inputDataList = invocationRequest.getInputDataList();
-  for (let inputBindingsIndex = 0; inputBindingsIndex < inputDataList.length; inputBindingsIndex++) {
-    let parameterBinding = inputDataList[inputBindingsIndex];
-    let name = parameterBinding.getName();
-    let typedData = parameterBinding.getData();
-
-    // if (typedData.typeVal == messages.TypedData.Type.Http) {
-    // TODO figure out how to use enum
-    if (typedData) {
-      if (typedData.getTypeVal() == 4) {
-        context.req = grpcMessageConverters.convertFromRpcHttp(typedData.getHttpVal());
+  for (let binding of invocationRequest.inputData || []) {
+    if (binding.data && binding.name) {
+      let input;
+      if (binding.data.typeVal == messages.TypedData.Type.Http) {
+        input = context.req = grpcMessageConverters.convertFromRpcHttp(binding.data.httpVal);
         context._triggerType = 'httptrigger';
-        // TODO figure out webHookTrigger
-        if (name != 'webhookReq') {
-          context._inputs.push(context.req);
-          context.bindings[name] = context.req;
-        }
       } else {
-        let inputDataValue = grpcMessageConverters.convertFromTypedDataObject(typedData);
-        context._inputs.push(inputDataValue);
-        context.bindings[name] = inputDataValue;
+        input = grpcMessageConverters.convertFromTypedData(binding.data);
       }
+      context._inputs.push(input);
+      context.bindings[binding.name] = input;
     }
   }
 
   context.bind = function (p, callback) {
-    for (let key in p) {
-      if (p.hasOwnProperty(key)) {
-        context.bindings[key] = p[key];
-      }
-    }
+    Object.assign(context.bindings, p);
     if (typeof callback === 'function') {
       callback(null);
     }
   };
 
   var resultCallback = function (error, result) {
+    let status: messages.StatusResult$Properties = {
+      status: messages.StatusResult.Status.Success
+    };
+
     if (error) {
-      statusResult.setStatus(messages.StatusResult.Status.Failure);
-      statusResult.setResult(error.toString());
-      invocationResponse.setResult(statusResult);
+      status.status = messages.StatusResult.Status.Failure;
+      status.result = error.toString();
     }
+
     if (result) {
-      statusResult.setStatus(messages.StatusResult.Status.Success);
-      statusResult.setResult(result.toString());
-      let returnParamerterBinding = new messages.ParameterBinding();
-      returnParamerterBinding.setName('$return');
-      returnParamerterBinding.setData(grpcMessageConverters.getTypedDataFromObject(result));
-      invocationResponse.setResult(statusResult);
-      invocationResponse.addOutputData(returnParamerterBinding);
+      context.bindings.$return = result;
+      status.result = result.toString();
     }
 
-    for (let key in context.bindings) {
-      let outputParameterBinding = new messages.ParameterBinding();
-      outputParameterBinding.setName(key);
-      if (key === 'req' || key === 'request' || key == 'res') {
-        let typedData = new messages.TypedData();
-        // typedData.setTypeVal(messages.TypedData.Type.Http);
-        typedData.setTypeVal(4);
-        typedData.setHttpVal(grpcMessageConverters.convertToRpcHttp(context.bindings[key]));
-        outputParameterBinding.setData(typedData);
-      } else {
-        outputParameterBinding.setData(grpcMessageConverters.getTypedDataFromObject(context.bindings[key]));
+    let response: messages.InvocationResponse$Properties = {
+      invocationId: invocationRequest.invocationId,
+      result: status
+    }
+
+    response.outputData = Object.keys(context.bindings)
+      .map(key => {
+        let typedData: messages.TypedData$Properties;
+        if (key == 'res') {
+          typedData = {
+            typeVal: messages.TypedData.Type.Http,
+            httpVal: grpcMessageConverters.convertToRpcHttp(context.bindings[key])
+          }
+        } else {
+          typedData = grpcMessageConverters.getTypedDataFromObject(context.bindings[key]);
+        }
+        return <messages.ParameterBinding$Properties>{
+          name: key,
+          data: typedData
+        }
+      });
+
+    var verification = messages.InvocationResponse.verify(response);
+
+    call.write({
+      requestId: requestId,
+      type: messages.StreamingMessage.Type.InvocationResponse,
+      content: {
+        type_url: 'type.googleapis.com/FunctionRpc.InvocationResponse',
+        value: messages.InvocationResponse.encode(response).finish()
       }
-      invocationResponse.addOutputData(outputParameterBinding);
-    }
-    statusResult.setStatus(messages.StatusResult.Status.Success);
-
-    // final response with the result
-
-    let invocationResponseStreamingMessage = new messages.StreamingMessage();
-    invocationResponseStreamingMessage.setRequestId(requestId);
-    invocationResponseStreamingMessage.setType(messages.StreamingMessage.Type.INVOCATIONRESPONSE);
-    invocationResponseStreamingMessage.setContent(grpcMessageConverters.getPackedMessage(invocationResponse, 'InvocationResponse'));
-    call.write(invocationResponseStreamingMessage);
+    });
 
     return result;
   };
 
   context.log = function (traceMessage) {
-    let logMessage = new messages.RpcLog();
-    logMessage.setInvocationId(context.invocationId);
-    logMessage.setCategory('Invocation');
-    logMessage.setMessage(JSON.stringify(traceMessage));
-
-    let logStreamingMessage = new messages.StreamingMessage();
-    logStreamingMessage.setType(messages.StreamingMessage.Type.RPCLOG);
-    logStreamingMessage.setContent(grpcMessageConverters.getPackedMessage(logMessage, 'RpcLog'));
-
-    call.write(logStreamingMessage);
-
-    // console.log('traceMessage: ' + JSON.stringify(traceMessage));
+    call.write({
+      type: messages.StreamingMessage.Type.RpcLog,
+      content: {
+        type_url: 'type.googleapis.com/FunctionRpc.RpcLog',
+        value: messages.RpcLog.encode({
+          invocationId: context.invocationId,
+          category: 'Invocation',
+          message: JSON.stringify(traceMessage)
+        }).finish()
+      }
+    });
   };
 
   context.handleUncaughtException = function (errorStack) {
     // TODO Log and self Terminate?
     if (errorStack) {
       console.log(errorStack);
-      let exceptionMessage = new messages.RpcException();
-      exceptionMessage.setStackTrace(errorStack);
-
-      statusResult.setStatus(messages.StatusResult.Status.Failure);
-      statusResult.setException(exceptionMessage);
-      invocationResponse.setResult(statusResult);
-
-      call.write(grpcMessageConverters.getPackedMessage(invocationResponse, 'InvocationResponse'));
       // process.exit(1);
     }
   };
 
   let azureFunctionScript = createFunction(require(scriptFilePath));
-  let azureFunctionScriptContext = [context, resultCallback];
-  var invokeFunctionCode = azureFunctionScript.apply(invokeFunctionCode, azureFunctionScriptContext);
-}
-
-function buildFunctionLoadResponse(requestId, functionId) {
-  let statusResult = new messages.StatusResult();
-  statusResult.setStatus(messages.StatusResult.Status.SUCCESS);
-  statusResult.setResult('Loaded function');
-
-  let functionLoadResponseMessage = new messages.FunctionLoadResponse();
-  functionLoadResponseMessage.setFunctionId(functionId);
-  functionLoadResponseMessage.setResult(statusResult);
-
-  let antTypeMessage = new proto.google.protobuf.Any();
-  antTypeMessage.pack(functionLoadResponseMessage.serializeBinary(), 'FunctionRpc.FunctionLoadResponse');
-
-  let functionLoadResponseStreamingMessage = new messages.StreamingMessage();
-  functionLoadResponseStreamingMessage.setRequestId(requestId);
-  functionLoadResponseStreamingMessage.setType(messages.StreamingMessage.Type.FUNCTIONLOADRESPONSE);
-  functionLoadResponseStreamingMessage.setContent(antTypeMessage);
-  return functionLoadResponseStreamingMessage;
-}
-
-function buildStartStream(requestId) {
-  let startStreamingMessage = new messages.StreamingMessage();
-  let emptyStartMessage = new messages.StartStream();
-
-  let anyEmptyStartMessage = new proto.google.protobuf.Any();
-  anyEmptyStartMessage.pack(emptyStartMessage.serializeBinary(), 'FunctionRpc.StartStream');
-
-  startStreamingMessage.setRequestId(requestId);
-  startStreamingMessage.setType(messages.StreamingMessage.Type.STARTSTREAM);
-  startStreamingMessage.setContent(anyEmptyStartMessage);
-  return startStreamingMessage;
+  azureFunctionScript(context, resultCallback);
 }
 
 module.exports = {
   handleFunctionLoadRequest: handleFunctionLoadRequest,
   handleInvokeRequest: handleInvokeRequest,
-  buildFunctionLoadResponse: buildFunctionLoadResponse,
-  buildStartStream: buildStartStream
 };
