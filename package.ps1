69031e4f8c7100dab40e0b4f3d25111739577b0a
Matt Mason <mamaso@microsoft.com>
2017-09-23 17:57:08 -0700

    bundle win32, darwin, linux

--- package.ps1
+++ package.ps1
@@ -5,14 +5,15 @@ mkdir ./pkg/grpc/etc
 mkdir ./pkg/dist/src
 copy-item ./node_modules/grpc/etc/roots.pem ./pkg/grpc/etc/
 copy-item ./node_modules/grpc/package.json ./pkg/grpc/
-copy-item ./getNativeFile.js ./pkg/grpc/
 copy-item ./dist/src/nodejsWorker.js ./pkg/dist/src/
 ./node_modules/.bin/webpack
-./node_modules/.bin/node-pre-gyp install -C pkg/grpc --target_arch=ia32 --target=8.4.0
-./node_modules/.bin/node-pre-gyp install -C pkg/grpc --target_arch=ia32 --target=7.7.4
-./node_modules/.bin/node-pre-gyp install -C pkg/grpc --target_arch=ia32 --target=6.11.2
+./node_modules/.bin/node-pre-gyp install -C pkg/grpc --target_arch=ia32 --target=8.4.0 --target_platform=win32
+./node_modules/.bin/node-pre-gyp install -C pkg/grpc --target_arch=ia32 --target=8.4.0 --target_platform=darwin
+./node_modules/.bin/node-pre-gyp install -C pkg/grpc --target_arch=ia32 --target=8.4.0 --target_platform=linux
+./node_modules/.bin/node-pre-gyp install -C pkg/grpc --target_arch=x64 --target=8.4.0 --target_platform=win32
+./node_modules/.bin/node-pre-gyp install -C pkg/grpc --target_arch=x64 --target=8.4.0 --target_platform=darwin
+./node_modules/.bin/node-pre-gyp install -C pkg/grpc --target_arch=x64 --target=8.4.0 --target_platform=linux
 copy-item Worker.nuspec pkg/
-copy-item Microsoft.Azure.Functions.NodeJsWorker.targets pkg/
 set-location pkg
 nuget pack -Properties version=$env:APPVEYOR_BUILD_NUMBER
 set-location ..
