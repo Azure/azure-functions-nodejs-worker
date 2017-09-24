27b1d5c38bb5a645ce26c854f104291d88bb9d1d
Matt Mason <mamaso@microsoft.com>
2017-09-23 17:19:41 -0700

    Update install scripts

--- getNativeFile.js
+++ getNativeFile.js
@@ -3,7 +3,7 @@ try {
   nodeAbi = require('./lib/node_modules/node-abi');
 }
 catch (e) {
-  nodeAbi = require('./node_modules/node-abi');
+  nodeAbi = require('node-abi');
 }
 
 var abi = nodeAbi.getAbi(process.version, 'node');
