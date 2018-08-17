dcf939245dd9dd1c9861b0ae40216abf3a63072a
mhoeger <marie.hoeger@gmail.com>
2018-08-16 17:48:06 -0700

    removing things that cause buffer deprecation warnings

--- src/Converters.ts
+++ src/Converters.ts
@@ -48,7 +48,7 @@ export function fromTypedData(typedData?: rpc.ITypedData, convertStringToJson: b
     }
     return str;
   } else if (typedData.bytes) {
-    return new Buffer(typedData.bytes);
+    return Buffer.from(typedData.bytes);
   }
 }
 
--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,9 +1,9 @@
 const logPrefix = "LanguageWorkerConsoleLog";
 
 export function systemLog(message?: any, ...optionalParams: any[]) { 
-    console.log(logPrefix + message, optionalParams); 
+    console.log(logPrefix + message, ...optionalParams); 
 };
 
 export function systemError(message?: any, ...optionalParams: any[]) { 
-    console.error(logPrefix + message, optionalParams); 
+    console.error(logPrefix + message, ...optionalParams); 
 };
