e872c071d5434a5298ce84d1c0ea06a730c0b925
Marie Hoeger <marie.hoeger@gmail.com>
2018-09-06 12:01:24 -0700

    Do not JSON.parse() array types (#126)
    * do not deserialize arrays
    
    * don't deserialize arrays
    
    * fix typo
    
    * Adding tests to converter
    
    * adding tests for nested arrays

--- src/Converters.ts
+++ src/Converters.ts
@@ -88,8 +88,8 @@ function convertKeysToCamelCase(obj: any) {
   for (var key in obj) {
       let value = fromTypedData(obj[key]) || obj[key];
       let camelCasedKey = key.charAt(0).toLocaleLowerCase() + key.slice(1);
-      // If the value is a JSON object (and not http, which is already cased), convert keys to camel case
-      if (typeof value === 'object' && value && value.http == undefined) {
+      // If the value is a JSON object (and not array and not http, which is already cased), convert keys to camel case
+      if (!Array.isArray(value) && typeof value === 'object' && value && value.http == undefined) {
         output[camelCasedKey] = convertKeysToCamelCase(value);
       } else {
         output[camelCasedKey] = value;
