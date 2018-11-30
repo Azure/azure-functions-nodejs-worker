470a5d8f667801a88a56e1e6670d0b6477125e04
Marie Hoeger <mariehoeger@Matts-MacBook-Pro.local>
2018-11-29 18:16:23 -0800

    adding comments to interface and removing IHttpContext

--- src/public/Interfaces.ts
+++ src/public/Interfaces.ts
@@ -1,43 +1,124 @@
+/**
+ * The Azure Function that is exported (via module.exports or exports). This is the function that will 
+ * execute when triggered. It is recommended that you declare this as an async function that implicitly
+ * returns a Promise.
+ * @param context An IContext object passed to your function from the Azure Function runtime.
+ * @returns Any output bindings or nothing.
+ */
 export interface IFunction {
-    (context: IContext, ...args: any[]): void;
+    (context: IContext, ...args: any[]): Promise<any> | void;
 }
 
+/**
+ * The context object can be used for reading and setting data from bindings, writing logs, and using the 
+ * context.done callback when your exported function is declared synchronous. A context object is passed 
+ * to your function from the Azure Function runtime on function invocation.
+ */
 export interface IContext {
+    /**
+     * A unique GUID per function invocation and execution.
+     */
     invocationId: string;
+    /**
+     * Function execution metadata.
+     */
     executionContext: IExecutionContext;
+    /**
+     * Input and trigger binding data, as defined in function.json. Properties on this object are dynamically 
+     * generated and named based off of the "name" property in function.json.
+     */
     bindings: { [key: string]: any };
+    /**
+     * Trigger metadata and function invocation data.
+     */
     bindingData: { [key: string]: any };
+    /**
+     * Bindings your function uses, as defined in function.json.
+     */
     bindingDefinitions: IBindingDefinition[];
+    /**
+     * Calling directly allows you to write streaming function logs at the default trace level. 
+     */
     log: ILogger;
+    /**
+     * A callback function that signals to the runtime that your code has completed. If your function is synchronous,
+     * you must call context.done at the end of execution. If your function is asynchronous, you should not use this 
+     * callback.
+     * 
+     * @param err A user-defined error to pass back to the runtime. If present, your function execution will fail.
+     * @param result An object containing output binding data.
+     */
     done: IDoneCallback;
-}
-
-export interface IHttpContext extends IContext {
-    req: IRequest;
-    res: { [key: string]: any };
+    /**
+     * Properties of the HTTP request that was made to invoke this function. This property will be defined if you are
+     * using HTTP bindings.
+     */
+    req?: IRequest;
+    /**
+     * HTTP response object to return. This property is used with HTTP bindings.
+     */
+    res?: { [key: string]: any };
 }
 
 export interface IRequest {
+    /**
+     * HTTP request method used to invoke this function.
+     */
     method: string;
+    /**
+     * Request URL.
+     */
     url: string;
-    originalUrl: string;
+    /**
+     * HTTP request headers.
+     */
     headers: {[key:string]: string};
+    /**
+     * Query string parameter keys and values from the URL.
+     */
     query: {[key:string]: string};
+    /**
+     * Route parameter keys and values.
+     */
     params: {[key:string]: string};
+    /**
+     * The HTTP request body
+     */
     body?: any;
+    /**
+     * The HTTP request body as a UTF-8 string
+     */
     rawbody?: any;
-    get(field: string): string | undefined;
 }
 
 export interface IExecutionContext {
+    /**
+     * A unique GUID per function invocation and execution.
+     */
     invocationId: string;
+    /**
+     * The name of the function that is being invoked. The name of your function is always the same as the
+     * name of the corresponding function.json's parent directory. 
+     */
     functionName: string;
+    /**
+     * The directory your function is in (this is the parent directory of this function's function.json).
+     */
     functionDirectory: string;
 }
 
 export interface IBindingDefinition {
+    /**
+     * The name of your binding, as defined in function.json.
+     */
     name: string,
+    /**
+     * The type of your binding, as defined in function.json.
+     */
     type: string, 
+    /**
+     * The direction of your binding ('in' or 'out'), as defined in function.json.
+     */
     direction: string
 }
 
@@ -46,9 +127,21 @@ export interface ILog {
 }
   
 export interface ILogger extends ILog {
+    /**
+     * Writes to error level logging or lower.
+     */
     error: ILog;
+    /**
+     * Writes to warning level logging or lower.
+     */
     warn: ILog;
+    /**
+     * Writes to info level logging or lower.
+     */
     info: ILog;
+    /**
+     * Writes to verbose level logging.
+     */
     verbose: ILog;
 }
 
--- test/InterfacesTest.ts
+++ test/InterfacesTest.ts
@@ -1,7 +1,7 @@
 // Test typescript interfaces for ts compliation errors
 import * as azf from "../types/public/Interfaces";
 
-let runHttp: azf.IFunction = async function (context: azf.IHttpContext, req: azf.IRequest) {
+let runHttp: azf.IFunction = async function (context: azf.IContext, req: azf.IRequest) {
     context.log('JavaScript HTTP trigger function processed a request.');
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
@@ -16,16 +16,19 @@ let runHttp: azf.IFunction = async function (context: azf.IHttpContext, req: azf
     }
 }
 
-let runHttpExpressModel: azf.IFunction = async function (context: azf.IHttpContext, req: azf.IRequest) {
+// Assumes output binding is named '$return'
+let runHttpReturn: azf.IFunction = async function (context: azf.IContext, req: azf.IRequest) {
+    context.log('JavaScript HTTP trigger function processed a request.');
     if (req.query.name || (req.body && req.body.name)) {
-        context.res
-            .status(200)
-            .header("Content-Type","text/plain")
-            .send("Hello" + (req.query.name || req.body.name));
+        return {
+            status: "200",
+            body: "Hello " + (req.query.name || req.body.name)
+        };
     } else {
-        context.res
-            .status("400")
-            .send("Please pass a name on the query string or in the request body");
+        return {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
     }
 }
 
@@ -33,8 +36,8 @@ let runServiceBus: azf.IFunction = function (context: azf.IContext, myQueueItem:
     context.log('Node.js ServiceBus queue trigger function processed message', myQueueItem); 
     context.log.verbose('EnqueuedTimeUtc =', context.bindingData.enqueuedTimeUtc); 
     context.log.verbose('DeliveryCount =', context.bindingData.deliveryCount); 
-    context.log.verbose('MessageId =', context.bindingData.messageId); 
-    context.done(); 
+    context.log.verbose('MessageId =', context.bindingData.messageId);
+    context.done();
 };
 
-export { runHttp, runHttpExpressModel, runServiceBus };
+export { runHttp, runHttpReturn, runServiceBus };
