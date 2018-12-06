07a061ad5537ea6a3d9ec1efd4268903b079d73f
mhoeger <marie.hoeger@gmail.com>
2018-12-05 18:21:27 -0800

    Adding README and package.json to public types

--- test/InterfacesTest.ts
+++ test/InterfacesTest.ts
@@ -1,7 +1,7 @@
 // Test typescript interfaces for ts compliation errors
-import * as azf from "../types/public/Interfaces";
+import { IFunction, IContext, IRequest} from "../types/public/Interfaces";
 
-let runHttp: azf.IFunction = async function (context: azf.IContext, req: azf.IRequest) {
+let runHttp: IFunction = async function (context: IContext, req: IRequest) {
     context.log('JavaScript HTTP trigger function processed a request.');
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
@@ -16,28 +16,28 @@ let runHttp: azf.IFunction = async function (context: azf.IContext, req: azf.IRe
     }
 }
 
-// Assumes output binding is named '$return'
-let runHttpReturn: azf.IFunction = async function (context: azf.IContext, req: azf.IRequest) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    if (req.query.name || (req.body && req.body.name)) {
-        return {
-            status: "200",
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    } else {
-        return {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-}
-
-let runServiceBus: azf.IFunction = function (context: azf.IContext, myQueueItem: string) { 
+let runServiceBus: IFunction = function (context: IContext, myQueueItem: string) { 
     context.log('Node.js ServiceBus queue trigger function processed message', myQueueItem); 
     context.log.verbose('EnqueuedTimeUtc =', context.bindingData.enqueuedTimeUtc); 
     context.log.verbose('DeliveryCount =', context.bindingData.deliveryCount); 
     context.log.verbose('MessageId =', context.bindingData.messageId);
     context.done();
 };
+
+// Assumes output binding is named '$return'
+let runHttpReturn: IFunction = async function (context: IContext, req: IRequest) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    if (req.query.name || (req.body && req.body.name)) {
+        return {
+            status: "200",
+            body: "Hello " + (req.query.name || req.body.name)
+        };
+    } else {
+        return {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+}
 
 export { runHttp, runHttpReturn, runServiceBus };
---
+++ types/public/README.md
@@ -0,0 +1,40 @@
+These are type definitions for using TypeScript with Azure Functions.
+
+Although Azure Functions does not directly support functions written in TypeScript, running transpiled functions is supported. Read more [here](https://docs.microsoft.com/azure/azure-functions/functions-reference-node#configure-function-entry-point) on configuring the location and name of your Azure Function. 
+
+# Install
+Because this package only contains TypeScript type definitions, it should be saved under `devDependencies`.
+
+`npm install @azure/functions --save-dev`
+
+# Usage
+```javascript
+import { IFunction, IContext, IRequest} from "@azure/functions";
+
+const index: IFunction = async function (context: IContext, req: IRequest) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            status: "200",
+            body: "Hello " + (req.query.name || req.body.name)
+        };
+    } else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+}
+
+export { index };
+```
+
+# Versions
+Versioning of @azure/functions follows the release cadence of the [Azure Functions Node.js Worker](https://github.com/Azure/azure-functions-nodejs-worker/releases). You can find the Azure Functions Node.js Worker version of a given Function Runtime Version [here](https://github.com/Azure/azure-functions-host/releases).
+
+# Getting Started with Azure Functions
+For developing Azure Functions to run on Node.js, it is recommended that you use Visual Studio Code as your IDE. You can follow this tutorial to [create and deploy your first JavaScript function](https://docs.microsoft.com/azure/azure-functions/functions-create-first-function-vs-code).
+
+The [Azure Functions developer guide](https://docs.microsoft.com/azure/azure-functions/functions-reference) and the[JavaScrip-specific developer guide](https://docs.microsoft.com/azure/azure-functions/functions-reference-node) are good resources for understanding more involved development.
+
+
