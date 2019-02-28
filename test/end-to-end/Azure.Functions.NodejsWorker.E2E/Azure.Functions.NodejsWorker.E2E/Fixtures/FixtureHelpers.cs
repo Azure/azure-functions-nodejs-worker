ed6483b149b6c9681115e6c396827a8624c94ec5
Katy Shimizu <kashimiz@microsoft.com>
2019-02-27 18:55:34 -0800

    Refactored end-to-end tests to use xUnit's collection fixtures.

---
+++ test/end-to-end/Azure.Functions.NodejsWorker.E2E/Azure.Functions.NodejsWorker.E2E/Fixtures/FixtureHelpers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Azure.Functions.NodeJs.Tests.E2E
+{
+    public static class FixtureHelpers
+    {
+        public static Process GetFuncHostProcess(bool enableAuth = false)
+        {
+            var funcProcess = new Process();
+            var rootDir = Path.GetFullPath(@"..\..\..\..\..\..\..");
+
+            funcProcess.StartInfo.UseShellExecute = false;
+            funcProcess.StartInfo.CreateNoWindow = true;
+            funcProcess.StartInfo.WorkingDirectory = Path.Combine(rootDir, @"test\end-to-end\testFunctionApp");
+            funcProcess.StartInfo.FileName = Path.Combine(rootDir, @"Azure.Functions.Cli\func.exe");
+            funcProcess.StartInfo.ArgumentList.Add("start");
+            if (enableAuth)
+            {
+                funcProcess.StartInfo.ArgumentList.Add("--enableAuth");
+            }
+
+            return funcProcess;
+        }
+
+        public static void KillExistingFuncHosts()
+        {
+            foreach (var func in Process.GetProcessesByName("func"))
+            {
+                func.Kill();
+            }
+        }
+    }
+}
