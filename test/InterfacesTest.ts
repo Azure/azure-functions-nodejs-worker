4203eb72b6301db53441b7802ce65457693c3c0b
mhoeger <marie.hoeger@gmail.com>
2019-04-01 10:35:01 -0700

    remove unnecessary import

--- test/InterfacesTest.ts
+++ test/InterfacesTest.ts
@@ -1,6 +1,5 @@
 // Test typescript interfaces for ts compliation errors
 import { AzureFunction, Context, HttpRequest, HttpMethod } from "../types/public/Interfaces";
-import { read } from "fs";
 
 const runHttp: AzureFunction = async function (context: Context, req: HttpRequest) {
     if (req.method === HttpMethod.GET) {
