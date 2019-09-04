0cb048542c6e80cac755dbd61c5b4d371478aea4
Marie Hoeger <marie.hoeger@gmail.com>
2019-09-04 19:39:45 +0200

    Log exception stack for uncaught exceptions (#241)
    * Log full stack on uncaught exceptions
    
    * updating dependencies
    
    * update package-lock
    
    * make sure errors without stack trace still logged

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,13 +1,20 @@
 const logPrefix = "LanguageWorkerConsoleLog";
 
 export function systemLog(message?: any, ...optionalParams: any[]) { 
-    console.log(logPrefix + message, ...optionalParams); 
+    console.log(logPrefix + removeNewLines(message), ...optionalParams); 
 };
 
 export function systemWarn(message?: any, ...optionalParams: any[]) { 
-    console.warn(logPrefix + "[warn] " + message, ...optionalParams); 
+    console.warn(logPrefix + "[warn] " + removeNewLines(message), ...optionalParams); 
 };
 
 export function systemError(message?: any, ...optionalParams: any[]) { 
-    console.error(logPrefix + "[error] " + message, ...optionalParams); 
+    console.error(logPrefix + "[error] " + removeNewLines(message), ...optionalParams); 
 };
+
+function removeNewLines(message?: any): string {
+    if (message && typeof message === 'string') {
+        message = message.replace(/(\r\n|\n|\r)/gm, " ");
+    }
+    return message;
+}
