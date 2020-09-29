9fbf7a3ba66f81ec423d086a30ef2d38db8ac792
Marie Hoeger <marie.hoeger@gmail.com>
2020-09-29 09:41:50 -0700

    Repopulate query / headers if missing from trigger metadata (#332)
    * initial changes to context
    
    * fill query and headers if missing

--- test/end-to-end/testFunctionApp/HttpTriggerBindingData/index.js
+++ test/end-to-end/testFunctionApp/HttpTriggerBindingData/index.js
@@ -2,9 +2,21 @@ module.exports = async function (context) {
     context.log('JavaScript HTTP trigger function processed a request.');
     const { methodName, utcNow, randGuid } = context.bindingData.sys;
     const { invocationId, query, headers } = context.bindingData;
-    const bindingDataExists = exists(methodName) && exists(utcNow) && exists(randGuid) && exists(invocationId) && exists(query) && exists(headers);
+    const bindingDataExists = 
+        exists(methodName) 
+        && exists(utcNow)
+        && exists(randGuid)
+        && exists(invocationId)
+        && exists(query)
+        && exists(headers)
+        && exists(query.stringInput)
+        && exists(query.emptyStringInput); // should be ""
     if (bindingDataExists) {
         context.res.body = "binding data exists"
+    } else {
+        context.res = {
+            status: 500
+        }
     }
 };
 
