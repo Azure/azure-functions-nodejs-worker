907880e63bde4c2199704a5a0a305a8469d793b1
mhoeger <mahoeger@microsoft.com>
2020-10-26 15:26:53 -0700

    revert changes

--- test/WorkerChannelTests.ts
+++ test/WorkerChannelTests.ts
@@ -6,7 +6,6 @@ import * as sinon from 'sinon';
 import { AzureFunctionsRpcMessages as rpc } from '../azure-functions-language-worker-protobuf/src/rpc';
 import 'mocha';
 import { load } from 'grpc';
-<<<<<<< HEAD
 import { FunctionInfo } from '../src/FunctionInfo';
 
 describe('WorkerChannel', () => {
@@ -86,75 +85,6 @@ describe('WorkerChannel', () => {
       test: orchestrationTriggerBinding
     }
   };
-=======
-import { worker } from 'cluster';
-
-describe('WorkerChannel', () => {
-  var channel: WorkerChannel;
-  var stream: TestEventStream;
-  var loader: sinon.SinonStubbedInstance<FunctionLoader>;
-  var functions;
-  
-  const runInvokedFunction = () => {
-    const triggerDataMock: { [k: string]: rpc.ITypedData } = {
-      "Headers": {
-          json: JSON.stringify({Connection: 'Keep-Alive'})
-      },
-      "Sys": {
-          json: JSON.stringify({MethodName: 'test-js', UtcNow: '2018', RandGuid: '3212'})
-        }
-    };
-
-    const inputDataValue = {
-      name: "req",
-      data: {
-          data: "http",
-          http: 
-          {
-              body:
-              {
-                  data: "string",
-                  body: "blahh"
-              },
-              rawBody:
-              {
-                  data: "string",
-                  body: "blahh"
-              }
-          }
-      } 
-    };
-
-    const actualInvocationRequest: rpc.IInvocationRequest = <rpc.IInvocationRequest> {
-      functionId: 'id',
-      invocationId: '1',
-      inputData: [inputDataValue],
-      triggerMetadata: triggerDataMock,
-    };
-
-    stream.addTestMessage({
-      invocationRequest: actualInvocationRequest
-    });
-    
-    return [inputDataValue, actualInvocationRequest];
-  }
-  
-  const assertInvokedFunction = (inputDataValue, actualInvocationRequest) => {
-    sinon.assert.calledWithMatch(stream.written, <rpc.IStreamingMessage> {
-      invocationResponse: {
-        invocationId: '1',
-        result:  {
-          status: rpc.StatusResult.Status.Success
-        },
-        outputData: []
-      }
-    });
-
-    // triggerMedata will be augmented with inpuDataValue since "RpcHttpTriggerMetadataRemoved" capability is set to true and therefore not populated by the host.
-    expect(JSON.stringify(actualInvocationRequest.triggerMetadata!.$request)).to.equal(JSON.stringify(inputDataValue.data));
-    expect(JSON.stringify(actualInvocationRequest.triggerMetadata!.req)).to.equal(JSON.stringify(inputDataValue.data));
-  }
->>>>>>> 0ff6fb4... feature: add before/after InvocationRequest hooks (#339)
 
   beforeEach(() => {
     stream = new TestEventStream();
@@ -455,7 +385,6 @@ describe('WorkerChannel', () => {
       channel['_invocationRequestBefore'] = [];
     });
 
-<<<<<<< HEAD
   it ('invokes function', () => {
     loader.getFunc.returns((context) => context.done());
     loader.getInfo.returns(new FunctionInfo(orchestratorBinding));
@@ -606,73 +535,6 @@ describe('WorkerChannel', () => {
       inputData: [httpInputData],
       triggerMetadata: getTriggerDataMock(),
     };
-=======
-    it("should apply hook before user function is executed", () => {
-      channel.registerBeforeInvocationRequest((context, userFunction) => {
-        context['magic_flag'] = 'magic value';
-        return userFunction.bind({ __wrapped: true });
-      });
-      
-      channel.registerBeforeInvocationRequest((context, userFunction) => {
-        context["secondary_flag"] = 'magic value';
-        return userFunction;
-      });
-
-      loader.getFunc.returns(function (this: any, context) {
-        expect(context['magic_flag']).to.equal('magic value');
-        expect(context['secondary_flag']).to.equal('magic value');
-        expect(this.__wrapped).to.equal(true);
-        expect(channel['_invocationRequestBefore'].length).to.equal(2);
-        expect(channel['_invocationRequestAfter'].length).to.equal(0);
-        context.done();
-      });
-      loader.getInfo.returns({
-        name: 'test',
-        outputBindings: {}
-      });
-
-      const [inputDataValue, actualInvocationRequest] = runInvokedFunction();
-      assertInvokedFunction(inputDataValue, actualInvocationRequest);
-    });
-    
-    it('should apply hook after user function is executed (callback)', (done) => {
-      let finished = false;
-      let count = 0;
-      channel.registerAfterInvocationRequest((context) => {
-        expect(finished).to.equal(true);
-        count += 1;
-      });
-
-      loader.getFunc.returns(function (this: any, context) {
-        finished = true;
-        expect(channel['_invocationRequestBefore'].length).to.equal(0);
-        expect(channel['_invocationRequestAfter'].length).to.equal(1);
-        expect(count).to.equal(0);
-        context.done();
-        expect(count).to.equal(1);
-        done();
-      });
-      loader.getInfo.returns({
-        name: 'test',
-        outputBindings: {}
-      });
-
-      const [inputDataValue, actualInvocationRequest] = runInvokedFunction();
-      assertInvokedFunction(inputDataValue, actualInvocationRequest);
-    });
-    
-    it('should apply hook after user function resolves (promise)', (done) => {
-      let finished = false;
-      let count = 0;
-      let inputDataValue, actualInvocationRequest;
-      channel.registerAfterInvocationRequest((context) => {
-        expect(finished).to.equal(true);
-        count += 1;
-        expect(count).to.equal(1);
-        assertInvokedFunction(inputDataValue, actualInvocationRequest);
-        done();
-      });
->>>>>>> 0ff6fb4... feature: add before/after InvocationRequest hooks (#339)
 
       loader.getFunc.returns(() => new Promise((resolve) => {
         finished = true;
@@ -701,7 +563,6 @@ describe('WorkerChannel', () => {
         done();
       });
 
-<<<<<<< HEAD
     sinon.assert.calledWithMatch(stream.written, <rpc.IStreamingMessage> {
       invocationResponse: {
         invocationId: '1',
@@ -850,33 +711,6 @@ describe('WorkerChannel', () => {
         }
       }
     });
-=======
-      loader.getFunc.returns((context) => new Promise((_, reject) => {
-        finished = true;
-        expect(channel['_invocationRequestBefore'].length).to.equal(0);
-        expect(channel['_invocationRequestAfter'].length).to.equal(1);
-        expect(count).to.equal(0);
-        reject();
-      }));
-      loader.getInfo.returns({
-        name: 'test',
-        outputBindings: {}
-      });
-
-      const [inputDataValue, actualInvocationRequest] = runInvokedFunction();
-    });
-  });
-
-  it ('invokes function', () => {
-    loader.getFunc.returns((context) => context.done());
-    loader.getInfo.returns({
-      name: 'test',
-      outputBindings: {}
-    })
-
-    const [inputDataValue, actualInvocationRequest] = runInvokedFunction();
-    assertInvokedFunction(inputDataValue, actualInvocationRequest);
->>>>>>> 0ff6fb4... feature: add before/after InvocationRequest hooks (#339)
   });
 
   it ('throws for malformed messages', () => {
