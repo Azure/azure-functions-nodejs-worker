04c26a055f4ab9b8e126daeb96e32967da3e5ff4
David Justo <david.justo.1996@gmail.com>
2021-05-25 16:06:44 -0700

    Minimize diffs in WorkerChannelTests

--- test/WorkerChannelTests.ts
+++ test/WorkerChannelTests.ts
@@ -684,46 +684,7 @@ describe('WorkerChannel', () => {
 
       sendInvokeMessage([httpInputData], getHttpTriggerDataMock());
     });
-    
-    it ('returns and serializes falsy value in Durable: ""', () => {
-      loader.getFunc.returns((context) => context.done(null, ""));
-      loader.getInfo.returns(new FunctionInfo(activityBinding));
-    
-      sendInvokeMessage([], getHttpTriggerDataMock());
-    
-      const expectedOutput = [];
-      const expectedReturnValue = { 
-        string: ""
-      };
-      assertInvocationSuccess(expectedOutput, expectedReturnValue);
-    });
-    
-    it ('returns and serializes falsy value in Durable: 0', () => {
-      loader.getFunc.returns((context) => context.done(null, 0));
-      loader.getInfo.returns(new FunctionInfo(activityBinding));
-    
-      sendInvokeMessage([], getHttpTriggerDataMock());
-    
-      const expectedOutput = [];
-      const expectedReturnValue = { 
-        int: 0
-      };
-      assertInvocationSuccess(expectedOutput, expectedReturnValue);
-    });
-    
-    it ('returns and serializes falsy value in Durable: false', () => {
-      loader.getFunc.returns((context) => context.done(null, false));
-      loader.getInfo.returns(new FunctionInfo(activityBinding));
-    
-      sendInvokeMessage([], getHttpTriggerDataMock());
-    
-      const expectedOutput = [];
-      const expectedReturnValue = { 
-        json: "false"
-      };
-      assertInvocationSuccess(expectedOutput, expectedReturnValue)
-    });
-    
+
     it('responds to worker status', async () => {
       stream.addTestMessage({
         requestId: 'id',
@@ -739,5 +700,46 @@ describe('WorkerChannel', () => {
       });
     });
 
+    it ('returns and serializes falsy value in Durable: ""', () => {
+      loader.getFunc.returns((context) => context.done(null, ""));
+      loader.getInfo.returns(new FunctionInfo(activityBinding));
+
+      sendInvokeMessage([], getHttpTriggerDataMock());
+
+      const expectedOutput = [];
+      const expectedReturnValue = { 
+        string: ""
+      };
+      assertInvocationSuccess(expectedOutput, expectedReturnValue);
+    });
+
+    it ('returns and serializes falsy value in Durable: 0', () => {
+      loader.getFunc.returns((context) => context.done(null, 0));
+      loader.getInfo.returns(new FunctionInfo(activityBinding));
+
+      sendInvokeMessage([], getHttpTriggerDataMock());
+
+      const expectedOutput = [];
+      const expectedReturnValue = { 
+        int: 0
+      };
+      assertInvocationSuccess(expectedOutput, expectedReturnValue);
+    });
+
+    it ('returns and serializes falsy value in Durable: false', () => {
+      loader.getFunc.returns((context) => context.done(null, false));
+      loader.getInfo.returns(new FunctionInfo(activityBinding));
+
+      sendInvokeMessage([], getHttpTriggerDataMock());
+
+      const expectedOutput = [];
+      const expectedReturnValue = { 
+        json: "false"
+      };
+      assertInvocationSuccess(expectedOutput, expectedReturnValue)
+    });
+
   });
+
+
 })
