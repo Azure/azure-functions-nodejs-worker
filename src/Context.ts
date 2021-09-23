e294f79cc5b31da3a08ac3a165897ef46d1b8bee
Alexey Rodionov <alrod@inbox.ru>
2021-09-22 18:23:20 -0700

    V4 only supports Node14 (#416)

--- src/Context.ts
+++ src/Context.ts
@@ -8,7 +8,7 @@ import LogCategory = rpc.RpcLog.RpcLogCategory;
 import { Context, ExecutionContext, Logger, BindingDefinition, HttpRequest, TraceContext } from './public/Interfaces' 
 import { v4 as uuid } from 'uuid'
 
-export function CreateContextAndInputs(info: FunctionInfo, request: rpc.IInvocationRequest, logCallback: LogCallback, callback: ResultCallback, v1WorkerBehavior: boolean) {
+export function CreateContextAndInputs(info: FunctionInfo, request: rpc.IInvocationRequest, logCallback: LogCallback, callback: ResultCallback) {
     const context = new InvocationContext(info, request, logCallback, callback);
 
     const bindings: Dict<any> = {};
@@ -21,7 +21,7 @@ export function CreateContextAndInputs(info: FunctionInfo, request: rpc.IInvocat
                 input = httpInput = fromRpcHttp(binding.data.http);
             } else {
                 // TODO: Don't hard code fix for camelCase https://github.com/Azure/azure-functions-nodejs-worker/issues/188
-                if (!v1WorkerBehavior && info.getTimerTriggerName() === binding.name) {
+                if (info.getTimerTriggerName() === binding.name) {
                     // v2 worker converts timer trigger object to camelCase
                     input = convertKeysToCamelCase(binding)["data"];
                 } else {
