2f3d4ba049de64f37679f724e6f588e12b9476bc
Eric Jizba <erijiz@microsoft.com>
2021-11-10 17:18:36 -0800

    Leverage dev ops task to install .NET 6
    ps1 script didn't work on other OS's

--- setup-e2e-tests.ps1
+++ setup-e2e-tests.ps1
@@ -21,10 +21,6 @@ if ($IsWindows) {
     }
 }
 
-Write-Host "Installing .NET 6..."
-Invoke-WebRequest 'https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1' -OutFile 'dotnet-install.ps1'
-./dotnet-install.ps1 -InstallDir "$env:ProgramFiles/dotnet" -Version "6.0.100-preview.7.21379.14" -Channel 'release'
-
 $FUNC_RUNTIME_VERSION = '4'
 $coreToolsDownloadURL = $null
 if ($UseCoreToolsBuildFromIntegrationTests.IsPresent)
