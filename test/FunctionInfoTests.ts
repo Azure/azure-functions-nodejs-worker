f7392cd458ab493ba2c8ee8a1ce67e8675731076
Eric Jizba <erijiz@microsoft.com>
2021-11-19 10:32:27 -0800

    Expand linting to include "test" and "types" folders (#447)

--- test/FunctionInfoTests.ts
+++ test/FunctionInfoTests.ts
@@ -1,64 +1,62 @@
-import { FunctionInfo } from '../src/FunctionInfo';
 import { expect } from 'chai';
-import * as sinon from 'sinon';
-import { AzureFunctionsRpcMessages as rpc } from '../azure-functions-language-worker-protobuf/src/rpc';
 import 'mocha';
+import { AzureFunctionsRpcMessages as rpc } from '../azure-functions-language-worker-protobuf/src/rpc';
+import { FunctionInfo } from '../src/FunctionInfo';
 
 describe('FunctionInfo', () => {
-  /** NullableBool */ 
-  it('gets $return output binding converter for http', () => {
-    let metadata: rpc.IRpcFunctionMetadata = {
-        bindings: {
-            req: {
-                type: "httpTrigger",
-                direction: 0,
-                dataType: 1
+    /** NullableBool */
+    it('gets $return output binding converter for http', () => {
+        const metadata: rpc.IRpcFunctionMetadata = {
+            bindings: {
+                req: {
+                    type: 'httpTrigger',
+                    direction: 0,
+                    dataType: 1,
+                },
+                $return: {
+                    type: 'http',
+                    direction: 1,
+                    dataType: 1,
+                },
             },
-            $return: {
-                type: "http",
-                direction: 1,
-                dataType: 1
-            }
-        }
-    };
-    
-    let funcInfo = new FunctionInfo(metadata);
-    expect(funcInfo.getReturnBinding().converter.name).to.equal("toRpcHttp");
-  });
+        };
 
-  it('"hasHttpTrigger" is true for http', () => {
-    let metadata: rpc.IRpcFunctionMetadata = {
-        bindings: {
-            req: {
-                type: "httpTrigger",
-                direction: 0,
-                dataType: 1
-            }
-        }
-    };
-    
-    let funcInfo = new FunctionInfo(metadata);
-    expect(funcInfo.getReturnBinding()).to.be.undefined;
-    expect(funcInfo.hasHttpTrigger).to.be.true;
-  });
+        const funcInfo = new FunctionInfo(metadata);
+        expect(funcInfo.getReturnBinding().converter.name).to.equal('toRpcHttp');
+    });
 
-  it('gets $return output binding converter for TypedData', () => {
-    let metadata: rpc.IRpcFunctionMetadata = {
-        bindings: {
-            input: {
-                type: "queue",
-                direction: 0,
-                dataType: 1
+    it('"hasHttpTrigger" is true for http', () => {
+        const metadata: rpc.IRpcFunctionMetadata = {
+            bindings: {
+                req: {
+                    type: 'httpTrigger',
+                    direction: 0,
+                    dataType: 1,
+                },
             },
-            $return: {
-                type: "queue",
-                direction: 1,
-                dataType: 1
-            }
-        }
-    };
-    
-    let funcInfo = new FunctionInfo(metadata);
-    expect(funcInfo.getReturnBinding().converter.name).to.equal("toTypedData");
-  });
-})
+        };
+
+        const funcInfo = new FunctionInfo(metadata);
+        expect(funcInfo.getReturnBinding()).to.be.undefined;
+        expect(funcInfo.hasHttpTrigger).to.be.true;
+    });
+
+    it('gets $return output binding converter for TypedData', () => {
+        const metadata: rpc.IRpcFunctionMetadata = {
+            bindings: {
+                input: {
+                    type: 'queue',
+                    direction: 0,
+                    dataType: 1,
+                },
+                $return: {
+                    type: 'queue',
+                    direction: 1,
+                    dataType: 1,
+                },
+            },
+        };
+        const funcInfo = new FunctionInfo(metadata);
+        expect(funcInfo.getReturnBinding().converter.name).to.equal('toTypedData');
+    });
+});
