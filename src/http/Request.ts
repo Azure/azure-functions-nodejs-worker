2a58f842f1c5c22b834dcda090452e35a9ee5efe
hossam-nasr <hossam.nasr99@gmail.com>
2021-12-01 14:29:18 -0800

    add AppService user to HttpRequest constructor

--- src/http/Request.ts
+++ src/http/Request.ts
@@ -1,7 +1,14 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License.
 
-import { HttpMethod, HttpRequest, HttpRequestHeaders, HttpRequestParams, HttpRequestQuery } from '@azure/functions';
+import {
+    HttpMethod,
+    HttpRequest,
+    HttpRequestHeaders,
+    HttpRequestParams,
+    HttpRequestQuery,
+    HttpRequestUser,
+} from '@azure/functions';
 import { AzureFunctionsRpcMessages as rpc } from '../../azure-functions-language-worker-protobuf/src/rpc';
 import { fromTypedData } from '../converters/RpcConverters';
 import { fromNullableMapping, fromRpcHttpBody } from '../converters/RpcHttpConverters';
@@ -13,6 +20,7 @@ export class Request implements HttpRequest {
     public headers: HttpRequestHeaders;
     public query: HttpRequestQuery;
     public params: HttpRequestParams;
+    public user?: HttpRequestUser;
     public body?: any;
     public rawBody?: any;
 
@@ -25,6 +33,17 @@ export class Request implements HttpRequest {
         this.params = fromNullableMapping(rpcHttp.nullableParams, rpcHttp.params);
         this.body = fromTypedData(<rpc.ITypedData>rpcHttp.body);
         this.rawBody = fromRpcHttpBody(<rpc.ITypedData>rpcHttp.body);
+        if (this.headers['x-ms-client-principal']) {
+            this.user = {
+                type: 'AppService',
+                id: this.headers['x-ms-client-principal-id'],
+                username: this.headers['x-ms-client-principal-name'],
+                identityProvider: this.headers['x-ms-client-principal-idp'],
+                claimsPrincipalData: JSON.parse(
+                    Buffer.from(this.headers['x-ms-client-principal'], 'base64').toString('utf-8')
+                ),
+            };
+        }
     }
 
     public get(field: string): string | undefined {
