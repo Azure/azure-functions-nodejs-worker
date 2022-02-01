d7eb0ff9b0b8c28bd364bb6decd181417bcbee2d
Hossam Mabed <31644556+hossam-nasr@users.noreply.github.com>
2022-01-31 16:56:22 -0800

    Automatically decode x-ms-client-principal header (#483)
    Add a user property to the HttpRequest object that contains information on the logged-in user, by decoding the x-ms-client-principal-* headers

--- src/http/Request.ts
+++ src/http/Request.ts
@@ -1,10 +1,18 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License.
 
-import { HttpMethod, HttpRequest, HttpRequestHeaders, HttpRequestParams, HttpRequestQuery } from '@azure/functions';
+import {
+    HttpMethod,
+    HttpRequest,
+    HttpRequestHeaders,
+    HttpRequestParams,
+    HttpRequestQuery,
+    HttpRequestUser,
+} from '@azure/functions';
 import { AzureFunctionsRpcMessages as rpc } from '../../azure-functions-language-worker-protobuf/src/rpc';
 import { fromTypedData } from '../converters/RpcConverters';
 import { fromNullableMapping, fromRpcHttpBody } from '../converters/RpcHttpConverters';
+import { extractHttpUserFromHeaders } from './extractHttpUserFromHeaders';
 
 export class Request implements HttpRequest {
     public method: HttpMethod | null;
@@ -16,6 +24,8 @@ export class Request implements HttpRequest {
     public body?: any;
     public rawBody?: any;
 
+    private _cachedUser?: HttpRequestUser | null;
+
     public constructor(rpcHttp: rpc.IRpcHttp) {
         this.method = <HttpMethod>rpcHttp.method;
         this.url = <string>rpcHttp.url;
@@ -25,6 +35,14 @@ export class Request implements HttpRequest {
         this.params = fromNullableMapping(rpcHttp.nullableParams, rpcHttp.params);
         this.body = fromTypedData(<rpc.ITypedData>rpcHttp.body);
         this.rawBody = fromRpcHttpBody(<rpc.ITypedData>rpcHttp.body);
+    }
+
+    public get user(): HttpRequestUser | null {
+        if (this._cachedUser === undefined) {
+            this._cachedUser = extractHttpUserFromHeaders(this.headers);
+        }
+
+        return this._cachedUser;
     }
 
     public get(field: string): string | undefined {
