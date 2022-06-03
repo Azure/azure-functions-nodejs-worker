30a773f14dbd7bebccae53c692aabb6ea3bffbca
hossam-nasr <hossam.nasr99@gmail.com>
2022-06-03 13:12:27 -0700

    make sure it compiles

--- test/eventHandlers/WorkerInitHandler.test.ts
+++ test/eventHandlers/WorkerInitHandler.test.ts
@@ -131,7 +131,6 @@ describe('WorkerInitHandler', () => {
     afterEach(async () => {
         mockFs.restore();
         await stream.afterEachEventHandlerTest();
-        channel.functionAppDirectory = '';
     });
 
     it('responds to init', async () => {
