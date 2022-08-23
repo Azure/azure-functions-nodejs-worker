07de890ab2d0f8b943880778709b5b6dc98b458a
hossam-nasr <hossam.nasr99@gmail.com>
2022-08-23 14:51:01 -0700

    add hooks tests

--- test/eventHandlers/terminateWorker.test.ts
+++ test/eventHandlers/terminateWorker.test.ts
@@ -8,11 +8,12 @@ import { WorkerChannel } from '../../src/WorkerChannel';
 import { Msg as AppStartMsg } from '../startApp.test';
 import { beforeEventHandlerSuite } from './beforeEventHandlerSuite';
 import { TestEventStream } from './TestEventStream';
+import { Msg as WorkerInitMsg } from './WorkerInitHandler.test';
 import sinon = require('sinon');
 import LogCategory = rpc.RpcLog.RpcLogCategory;
 import LogLevel = rpc.RpcLog.Level;
 
-namespace Msg {
+export namespace Msg {
     export function workerTerminate(gracePeriodSeconds = 5): rpc.IStreamingMessage {
         return {
             workerTerminate: {
@@ -36,12 +37,14 @@ describe('terminateWorker', () => {
     let stream: TestEventStream;
     let channel: WorkerChannel;
     let processExitStub: sinon.SinonStub;
+    let streamEndStub: sinon.SinonStub;
     let coreApi: typeof coreTypes;
     let testDisposables: coreTypes.Disposable[] = [];
 
     before(async () => {
         ({ channel, stream } = beforeEventHandlerSuite());
         processExitStub = sinon.stub(process, 'exit');
+        streamEndStub = sinon.stub(channel.eventStream, 'end');
         coreApi = await import('@azure/functions-core');
     });
 
@@ -50,6 +53,8 @@ describe('terminateWorker', () => {
         testDisposables = [];
         channel.appHookData = {};
         channel.appLevelOnlyHookData = {};
+        processExitStub.resetHistory();
+        streamEndStub.resetHistory();
         await stream.afterEachEventHandlerTest();
     });
 
@@ -63,7 +68,13 @@ describe('terminateWorker', () => {
         expect(processExitStub.calledWith(0)).to.be.true;
     });
 
-    it('shuts down worker in response to worker_termiante request', async () => {
+    it('ends event stream', async () => {
+        stream.addTestMessage(Msg.workerTerminate());
+        await stream.assertCalledWith(Msg.receivedWorkerTerminateLog);
+        expect(streamEndStub.callCount).to.be.equal(1);
+    });
+
+    it('shuts down worker process', async () => {
         stream.addTestMessage(Msg.workerTerminate());
         await stream.assertCalledWith(Msg.receivedWorkerTerminateLog);
         expect(processExitStub.calledWith(0)).to.be.true;
@@ -87,5 +98,118 @@ describe('terminateWorker', () => {
         );
         expect(hookFunc.callCount).to.be.equal(1);
         expect(hookFunc.args[0][0]).to.deep.equal(expectedContext);
+    });
+
+    it('runs more than one app terminate hook', async () => {
+        const gracePeriod = 5;
+        const expectedContext: coreTypes.AppTerminateContext = {
+            gracePeriod,
+            hookData: {},
+            appHookData: {},
+        };
+        const hookFunc = sinon.spy();
+        const hookFun2 = sinon.spy();
+        testDisposables.push(coreApi.registerHook('appTerminate', hookFunc));
+        testDisposables.push(coreApi.registerHook('appTerminate', hookFun2));
+
+        stream.addTestMessage(Msg.workerTerminate(gracePeriod));
+        await stream.assertCalledWith(
+            Msg.receivedWorkerTerminateLog,
+            AppStartMsg.executingHooksLog(2, 'appTerminate'),
+            AppStartMsg.executedHooksLog('appTerminate')
+        );
+        expect(hookFunc.callCount).to.be.equal(1);
+        expect(hookFunc.args[0][0]).to.deep.equal(expectedContext);
+        expect(hookFun2.callCount).to.be.equal(1);
+        expect(hookFun2.args[0][0]).to.deep.equal(expectedContext);
+    });
+
+    it('allows app terminate hooks to share data', async () => {
+        let hookData = '';
+        testDisposables.push(
+            coreApi.registerHook('appTerminate', (context) => {
+                context.hookData.hello = 'world';
+                context.appHookData.foo = 'bar';
+                hookData += 'term1';
+            })
+        );
+        testDisposables.push(
+            coreApi.registerHook('appTerminate', (context) => {
+                expect(context.hookData.hello).to.equal('world');
+                expect(context.appHookData.foo).to.equal('bar');
+                hookData += 'term2';
+            })
+        );
+
+        stream.addTestMessage(Msg.workerTerminate());
+        await stream.assertCalledWith(
+            Msg.receivedWorkerTerminateLog,
+            AppStartMsg.executingHooksLog(2, 'appTerminate'),
+            AppStartMsg.executedHooksLog('appTerminate')
+        );
+        expect(hookData).to.equal('term1term2');
+    });
+
+    it('allows app start and app terminate hooks to share data', async () => {
+        let hookData = '';
+        testDisposables.push(
+            coreApi.registerHook('appStart', (context) => {
+                context.hookData.hello = 'world';
+                context.appHookData.foo = 'bar';
+                hookData += 'start';
+            })
+        );
+        testDisposables.push(
+            coreApi.registerHook('appTerminate', (context) => {
+                expect(context.hookData.hello).to.equal('world');
+                expect(context.appHookData.foo).to.equal('bar');
+                hookData += 'term';
+            })
+        );
+
+        stream.addTestMessage(WorkerInitMsg.init());
+        await stream.assertCalledWith(
+            WorkerInitMsg.receivedInitLog,
+            WorkerInitMsg.warning('Worker failed to load package.json: file does not exist'),
+            AppStartMsg.executingHooksLog(1, 'appStart'),
+            AppStartMsg.executedHooksLog('appStart'),
+            WorkerInitMsg.response
+        );
+
+        stream.addTestMessage(Msg.workerTerminate());
+        await stream.assertCalledWith(
+            Msg.receivedWorkerTerminateLog,
+            AppStartMsg.executingHooksLog(1, 'appTerminate'),
+            AppStartMsg.executedHooksLog('appTerminate')
+        );
+
+        expect(hookData).to.equal('startterm');
+    });
+
+    it('enforces readonly property of hookData and appHookData in hook contexts', async () => {
+        testDisposables.push(
+            coreApi.registerHook('appTerminate', (context) => {
+                expect(() => {
+                    // @ts-expect-error: setting readonly property
+                    context.hookData = {
+                        hello: 'world',
+                    };
+                }).to.throw('Attempting to set readonly property hookData');
+                expect(() => {
+                    // @ts-expect-error: setting readonly property
+                    context.appHookData = {
+                        hello: 'world',
+                    };
+                }).to.throw('Attempting to set readonly property appHookData');
+            })
+        );
+
+        stream.addTestMessage(Msg.workerTerminate());
+
+        await stream.assertCalledWith(
+            Msg.receivedWorkerTerminateLog,
+            AppStartMsg.executingHooksLog(1, 'appTerminate'),
+            AppStartMsg.executedHooksLog('appTerminate')
+        );
     });
 });
