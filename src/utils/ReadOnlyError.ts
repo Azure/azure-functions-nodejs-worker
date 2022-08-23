7d21748dc8f5ee1f4899601da934e048d29fc36b
hossam-nasr <hossam.nasr99@gmail.com>
2022-08-23 11:02:54 -0700

    make instance of TypeError

--- src/utils/ReadOnlyError.ts
+++ src/utils/ReadOnlyError.ts
@@ -1,10 +1,9 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License.
 
-import { InternalException } from './InternalException';
-
-export class ReadOnlyError extends InternalException implements TypeError {
+export class ReadOnlyError extends TypeError {
+    isAzureFunctionsInternalException = true;
     constructor(propertyName: string) {
-        super(`Cannot assign to read-only property '${propertyName}'`);
+        super(`Cannot assign to read only property '${propertyName}'`);
     }
 }
