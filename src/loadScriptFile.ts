98108e603834c5bcdb85082c1e51463ae610c6d9
Eric Jizba <erijiz@microsoft.com>
2022-08-25 14:03:22 -0700

    Clean up errors

--- src/loadScriptFile.ts
+++ src/loadScriptFile.ts
@@ -2,8 +2,8 @@
 // Licensed under the MIT License.
 
 import * as url from 'url';
+import { AzFuncSystemError } from './errors';
 import { PackageJson } from './parsers/parsePackageJson';
-import { InternalException } from './utils/InternalException';
 
 export async function loadScriptFile(filePath: string, packageJson: PackageJson): Promise<unknown> {
     let script: unknown;
@@ -13,7 +13,7 @@ export async function loadScriptFile(filePath: string, packageJson: PackageJson)
             // use eval so it doesn't get compiled into a require()
             script = await eval('import(fileUrl.href)');
         } else {
-            throw new InternalException(`'${filePath}' could not be converted to file URL (${fileUrl.href})`);
+            throw new AzFuncSystemError(`'${filePath}' could not be converted to file URL (${fileUrl.href})`);
         }
     } else {
         script = require(/* webpackIgnore: true */ filePath);
--- src/setupEventStream.ts
+++ src/setupEventStream.ts
@@ -2,14 +2,13 @@
 // Licensed under the MIT License.
 
 import { AzureFunctionsRpcMessages as rpc } from '../azure-functions-language-worker-protobuf/src/rpc';
+import { AzFuncSystemError, ensureErrorType } from './errors';
 import { EventHandler, SupportedRequest } from './eventHandlers/EventHandler';
 import { FunctionEnvironmentReloadHandler } from './eventHandlers/FunctionEnvironmentReloadHandler';
 import { FunctionLoadHandler } from './eventHandlers/FunctionLoadHandler';
 import { FunctionsMetadataHandler } from './eventHandlers/FunctionsMetadataHandler';
 import { InvocationHandler } from './eventHandlers/InvocationHandler';
 import { WorkerInitHandler } from './eventHandlers/WorkerInitHandler';
-import { ensureErrorType } from './utils/ensureErrorType';
-import { InternalException } from './utils/InternalException';
 import { systemError } from './utils/Logger';
 import { nonNullProp } from './utils/nonNull';
 import { WorkerChannel } from './WorkerChannel';
@@ -29,7 +28,7 @@ export function setupEventStream(workerId: string, channel: WorkerChannel): void
 
     channel.eventStream.on('error', function (err) {
         systemError(`Worker ${workerId} encountered event stream error: `, err);
-        throw new InternalException(err);
+        throw new AzFuncSystemError(err);
     });
 
     // wrap event stream write to validate message correctness
@@ -38,7 +37,7 @@ export function setupEventStream(workerId: string, channel: WorkerChannel): void
         const msgError = rpc.StreamingMessage.verify(msg);
         if (msgError) {
             systemError(`Worker ${workerId} malformed message`, msgError);
-            throw new InternalException(msgError);
+            throw new AzFuncSystemError(msgError);
         }
         oldWrite.apply(channel.eventStream, [msg]);
     };
@@ -85,7 +84,7 @@ async function handleMessage(workerId: string, channel: WorkerChannel, inMsg: rp
                 // Not yet implemented
                 return;
             default:
-                throw new InternalException(`Worker ${workerId} had no handler for message '${eventName}'`);
+                throw new AzFuncSystemError(`Worker ${workerId} had no handler for message '${eventName}'`);
         }
 
         request = nonNullProp(inMsg, eventName);
@@ -94,7 +93,7 @@ async function handleMessage(workerId: string, channel: WorkerChannel, inMsg: rp
         outMsg[eventHandler.responseName] = response;
     } catch (err) {
         const error = ensureErrorType(err);
-        if (error.isAzureFunctionsInternalException) {
+        if (error.isAzureFunctionsSystemError) {
             channel.log({
                 message: error.message,
                 level: LogLevel.Error,
