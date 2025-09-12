117de216e9a6d3c6b1abd50fc4fa3622b056bcf8
Evan Roman <evanroman@microsoft.com>
2025-09-12 11:23:05 -0500

    Add to init

--- src/eventHandlers/WorkerInitHandler.ts
+++ src/eventHandlers/WorkerInitHandler.ts
@@ -7,7 +7,7 @@ import { AzureFunctionsRpcMessages as rpc } from '../../azure-functions-language
 import { isError } from '../errors';
 import { startApp } from '../startApp';
 import { nonNullProp } from '../utils/nonNull';
-// import { validateNodeVersion } from '../utils/util';
+import { validateNodeVersion } from '../utils/util';
 import { worker } from '../WorkerContext';
 import { EventHandler } from './EventHandler';
 import { getWorkerCapabilities } from './getWorkerCapabilities';
@@ -44,7 +44,7 @@ export class WorkerInitHandler extends EventHandler<'workerInitRequest', 'worker
         });
 
         logColdStartWarning();
-        // validateNodeVersion(process.version);
+        validateNodeVersion(process.version);
 
         worker._hostVersion = nonNullProp(msg, 'hostVersion');
 
