04eab56b2ecc7ab8fc457e3a0babc8ea47edef7b
Tsuyoshi Ushio <tsushi@microsoft.com>
2026-05-13 15:51:27 -0700

    Switch to npm overrides pin for @protobufjs/inquire
    Replace the webpack alias shim with an npm overrides pin of @protobufjs/inquire to 1.1.0. Simpler, no performance overhead, and no custom code to maintain.
    
    Co-authored-by: Dobby <dobby@microsoft.com>

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,6 @@ module.exports = {
         library: 'worker',
         libraryTarget: 'commonjs2',
     },
-    resolve: {
-        alias: {
-            '@protobufjs/inquire': `${__dirname}/scripts/webpack-protobufjs-inquire.js`,
-        },
-    },
     target: 'node',
     node: {
         __dirname: false,
