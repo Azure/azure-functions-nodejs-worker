cce6ed935743251f7a98abddff0b166a89bfb25a
Tsuyoshi Ushio <tsushi@microsoft.com>
2026-08-27 08:29:45 -0700

    Handle prerelease Node.js versions
    Fixes #842
    
    Co-authored-by: Dobby <dobby@microsoft.com>

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -3,6 +3,7 @@
 
 import 'mocha';
 import { expect } from 'chai';
+import { AzureFunctionsRpcMessages as rpc } from '../azure-functions-language-worker-protobuf/src/rpc';
 import { getNodeVersionLog } from '../src/utils/util';
 
 describe('utils', () => {
@@ -11,6 +12,19 @@ describe('utils', () => {
             const result = getNodeVersionLog('v26.0.0');
 
             expect(result?.message ?? '').to.not.contain('Incompatible Node.js version v26');
+        });
+
+        it('warns for prerelease Node.js versions', () => {
+            const result = getNodeVersionLog('v26.8.0-alpha.0.0.0');
+
+            expect(result?.message).to.contain('Prerelease Node.js version v26.8.0-alpha.0.0.0');
+            expect(result?.level).to.equal(rpc.RpcLog.Level.Warning);
+        });
+
+        it('rejects invalid Node.js versions', () => {
+            expect(() => getNodeVersionLog('not-a-version')).to.throw(
+                "Could not parse Node.js version: 'not-a-version'"
+            );
         });
 
         it('warns for unknown Node.js versions', () => {
